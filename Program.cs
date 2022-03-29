5b3dadfe259601248dffde72f6913f44e2719889
devLuis-Duarte <luis.saldanha@aluno.ifsp.edu.br>
2022-03-29 15:08:31 -0300

    apresenta a situacao do individuo de acordo com o imc

--- Program.cs
+++ Program.cs
@@ -15,10 +15,26 @@ namespace calculadoraIMC
             Console.WriteLine("Qual o seu peso?");
             float peso = Convert.ToSingle(Console.ReadLine());
 
-            double imc = peso /(altura * altura);
+            double imc = Math.Round(peso /(altura * altura), 2);
 
             Console.WriteLine("{0} sua altura eh {1} seu peso {2}", nome, altura, peso);
             Console.WriteLine("{0} seu IMC ", imc);
+
+            if(imc <17){
+                Console.WriteLine("sua situacao e muito abaixo do peso");
+            } else if(imc >=17.00 && imc < 18.50){
+                Console.WriteLine("sua situacao e abaixo do peso");
+            } else if(imc >=18.50 && imc <25.00){
+                Console.WriteLine("sua situacao e peso normal");
+            } else if(imc >=25.00 && imc >= 30.00){
+                Console.WriteLine("sua situacao e acima de peso");
+            } else if (imc >=30.00 && imc <35.00){
+                Console.WriteLine("sua situacao e obesidade 1 ");
+            } else if(imc >= 35.00 && imc <40.00){
+                Console.WriteLine("sua situacao e obesidade 2");
+            } else {
+                Console.WriteLine("sua situacao e obesidade 3");
+            }
         }
     }
 }
